e5632dd90df85f74cbf608257773743705c07e2e
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-04 15:56:25 +0100

    update

--- R/code.R
+++ R/code.R
@@ -7,6 +7,7 @@
 library(tidyverse)
 library(munro)
 library(showtext)
+library(patchwork)
 
 # Comparisons ----
 
@@ -32,25 +33,71 @@ highland_munros <- tibble(
   mutate(ymin = c(0, max(ratio)),
          ymax = c(max(ratio), 1))
 
-ggplot(data = highland_munros,
-       aes(x = 1.5, y = ratio, fill = county)) +
+p1 <- ggplot(data = highland_munros,
+       aes(x = 4, y = ratio, fill = county)) +
   geom_bar(stat = "identity",
            show.legend = FALSE) +
   scale_fill_manual(values = c("#04aed9", "#c5e0f5")) +
   coord_polar(theta = "y", start = 0) +
-  xlim(c(0.025, 2.5)) +
+  xlim(c(0.025, 8)) +
   ggtitle(label = "How many Munros are located in the Highlands ?") +
-  annotate("text", x = 2.25, y = 0, label = "Munros are mountains in Scotland above 3,000 feet",
-           colour = "white", family = "Lobster", size = 8) +
-  annotate("text", x = 2.25, y = 0.5, label = "Out of 282 Munros, 186 are located in the Highlands",
-           colour = "white", family = "Lobster", size = 8) +
-  annotate("text", x = 2.5, y = 0.5, label = "Visualisation: Jonathan Kitt | Data source: munro package | #30DayChartChallenge 2022 | Day 1: part-to-whole",
-           colour = "white", size = 3) +
+  annotate("text", x = 7.5, y = 0, label = "Munros are mountains in Scotland above 3,000 feet",
+           colour = "white", family = "Lobster", size = 15) +
+  annotate("text", x = 6, y = 0, label = "Out of 282 Munros, 186 are located in the Highlands",
+           colour = "white", family = "Lobster", size = 15) +
+  annotate("text", x = 6, y = 0.5, label = "Visualisation: Jonathan Kitt | Data source: munro package | #30DayChartChallenge 2022 | Day 1: part-to-whole",
+           colour = "white", size = 8) +
   theme_void() +
   theme(panel.background = element_rect(fill = "#041f32", colour = "#041f32"),
         plot.background = element_rect(fill = "#041f32", colour = "#041f32"),
         plot.title = element_text(colour = "white", hjust = 0.5,
-                                  family = "Lobster", size = 30, margin = margin(t = 30)))
+                                  family = "Lobster", size = 50, margin = margin(t = 30)))
+
+ggsave("plots/01_part_to_whole.png", p1, dpi = 320, width = 12, height = 6)
+
+p1 <- ggplot(data = highland_munros,
+       aes(x = 4, y = ratio, fill = county)) +
+  geom_bar(stat = "identity",
+           show.legend = FALSE) +
+  scale_fill_manual(values = c("#04aed9", "#c5e0f5")) +
+  coord_polar(theta = "y", start = 0) +
+  xlim(c(0.025, 8)) +
+  # ggtitle(label = "How many Munros are located in the Highlands ?") +
+  # annotate("text", x = 7.5, y = 0, label = "Munros are mountains in Scotland above 3,000 feet",
+  #          colour = "white", family = "Lobster", size = 15) +
+  # annotate("text", x = 6, y = 0, label = "Out of 282 Munros, 186 are located in the Highlands",
+  #          colour = "white", family = "Lobster", size = 15) +
+  # annotate("text", x = 6, y = 0.5, label = "Visualisation: Jonathan Kitt | Data source: munro package | #30DayChartChallenge 2022 | Day 1: part-to-whole",
+  #          colour = "white", size = 8) +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#041f32", colour = "#041f32"),
+        plot.background = element_rect(fill = "#041f32", colour = "#041f32"),
+        plot.title = element_text(colour = "white", hjust = 0.5,
+                                  family = "Lobster", size = 50, margin = margin(t = 30)))
+
+p2 <- ggplot() +
+  annotate("text", x = -0.5, y = 1, label = "Munros are mountains in Scotland above 3,000 feet",
+           colour = "white", family = "Lobster", size = 20) +
+  annotate("text", x = 0.5, y = -1, label = "Out of 282 Munros, 186 are located in the Highlands",
+           colour = "white", family = "Lobster", size = 20) +
+  xlim(-15, 15) +
+  ylim(-5, 5) +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#041f32", colour = "#041f32"),
+        plot.background = element_rect(fill = "#041f32", colour = "#041f32"))
+
+p <- p2 + p1 +
+  plot_annotation(
+    title = "How many Munros are located in the Highlands?",
+    caption = "Visualisation: Jonathan Kitt | Data source: munro package | #30DayChartChallenge 2022 | Day 1: part-to-whole",
+    theme = theme(plot.title = element_text(family = "Lobster", colour = "white", size = 100, hjust = 0.5,
+                                            margin = margin(t = 40)),
+                  plot.background = element_rect(fill = "#041f32"),
+                  plot.caption = element_text(colour = "white", hjust = 0.5, size = 25)))
+
+ggsave("plots/01_part_to_whole.png", p, dpi = 320, width = 12, height = 6)
+
+
   
 # 2 - Pictogram
 # 3 - Historical
