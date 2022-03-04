bd3c4cf87eca15e5ade1a94c1ae0e9b385efe71d
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-04 10:24:59 +0100

    started 2022 challenge

--- R/code.R
+++ R/code.R
@@ -6,4 +6,15 @@
 
 library(tidyverse)
 
-# #1 Part-to-whole
+# 1 - Part-to-whole ----
+# 2 - Pictogram ----
+# 3 - Historical ----
+# 4 - Flora ----
+# 5 - Slope ----
+# 6 - Data day: OWID ----
+# 7 - Physical ----
+# 8 - Mountains ----
+# 9 - Statistics ----
+# 10 - Experimental ----
+# 11 - Circular ----
+# 12 - Theme day: The Economist ----
