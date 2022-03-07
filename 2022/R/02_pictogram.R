8dbf611590201c767af3d11b037c9d624c74845a
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-07 10:27:45 +0100

    update

--- 2022/R/02_pictogram.R
+++ 2022/R/02_pictogram.R
@@ -56,14 +56,3 @@ p <- ggplot(data = d1) +
   theme_void()
 
 ggsave("2022/plots/02_pictogram.png", p, dpi = 320, width = 12, height = 6)
-
-p <- p1 + p2 +
-  plot_annotation(
-    title = "How many Munros are located in the Highlands?",
-    caption = "Visualisation: Jonathan Kitt | Data source: munro package | #30DayChartChallenge 2022 | Day 1: part-to-whole",
-    theme = theme(plot.title = element_text(family = "Lobster", colour = "white", size = 100, hjust = 0.5,
-                                            margin = margin(t = 40)),
-                  plot.background = element_rect(fill = "#041f32"),
-                  plot.caption = element_text(colour = "white", hjust = 0.5, size = 25)))
-
-ggsave("2022/plots/02_pictogram.png", p, dpi = 320, width = 12, height = 6)
