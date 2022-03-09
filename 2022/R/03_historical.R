fd3e70b541cb8f7fbba7e1453174c484a9288e5c
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-09 15:05:41 +0100

    ascombe's data

--- 2022/R/03_historical.R
+++ 2022/R/03_historical.R
@@ -4,7 +4,7 @@
 # Day 3 : Historical
 # Last updated 2022-03-09
 
-# Source : https://blog.plotly.com/post/120532468127/how-to-analyze-data-seven-modern-remakes-of-the
+# Source : https://en.wikipedia.org/wiki/Anscombe%27s_quartet
 
 # Load packages ----
 
@@ -16,10 +16,30 @@ library(patchwork)
 
 # Load fonts ----
 
-font_add_google("Rajdhani", "Rajdhani")
-showtext_auto()
+# font_add_google("Rajdhani", "Rajdhani")
+# showtext_auto()
 
 # Data wrangling ----
+
+d1 <- tibble(
+  quartet_nb = rep(1:4, each = 11),
+  x = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
+        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
+        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
+        8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8),
+  y = c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68,
+        9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74,
+        7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73,
+        6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89))
+
+d1 %>% filter(quartet_nb == 2)
+
+d1 <- d1 %>% 
+  select(year = `Année de Year`, category = `Noms de mesures`, value = `Valeurs de mesures`)
+
+ggplot() +
+  geom_line(data = d1, aes(x = year, y = value, colour = category)) +
+  ylim(0, 200)
 
 d1 <- tibble(
   os = c(rep("Android", 71), rep("iOS", 28), "Other"))
