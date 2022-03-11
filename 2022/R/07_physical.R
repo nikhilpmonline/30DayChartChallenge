5b13a162f78989c9d358e7e9f3d43502829c0bd0
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-11 17:30:13 +0100

    plot 7 ok

--- 2022/R/07_physical.R
+++ 2022/R/07_physical.R
@@ -26,8 +26,9 @@ ggplot() +
 
 # Load fonts ----
 
-# font_add_google("Tangerine", "Tangerine")
-# showtext_auto()
+font_add_google("Goldman", "Goldman")
+font_add_google("Big Shoulders Inline Text", "Big Shoulders Inline Text")
+showtext_auto()
 
 # Import data ----
 
@@ -45,7 +46,7 @@ d1 <- planets %>%
                          TRUE ~ cum_dist + (rowid-1)*50e3 + radius)) %>% 
   mutate(planet = fct_inorder(planet))
 
-p <- ggplot() +
+p1 <- ggplot() +
   geom_circle(data = d1,
               aes(x0 = x.0, y0 = 0, r = radius, fill = planet),
               show.legend = FALSE) +
@@ -54,160 +55,47 @@ p <- ggplot() +
   coord_fixed() +
   geom_text(data = d1,
             aes(x = x.0, y = radius + 10e3, label = planet,
-                colour = planet)) +
+                colour = planet),
+            show.legend = FALSE, family = "Goldman", size = 12) +
   scale_colour_manual(values = c("#97979f", "#e3bb76", "#8cb1de", "#c1440e",
                                "#e3dccc", "#e2bf7b", "#afdbf5", "#3e66f9")) +
+  annotate("text", x = 100e3, y = 60e3, label = "Relative sizes", family = "Goldman", colour = "white",
+           size = 25) +
   theme_void() +
   theme(plot.background = element_rect(fill = "black", colour = "black"),
-        panel.background = element_rect(fill = "black", colour = "black"))
+        panel.background = element_rect(fill = "black", colour = "black"),
+        plot.margin = margin(b = 20))
+
+p2 <- ggplot() +
+  geom_segment(data = d1, aes(x = 0, xend = distance_from_sun, 
+                              y = planet, yend = planet),
+               show.legend = FALSE, colour = "#ffffff", size = 0.1) +
+  geom_point(data = d1, aes(x = distance_from_sun, y = planet, colour = planet),
+             size = 6, show.legend = FALSE)  +
+  geom_text(data = d1,
+            aes(x = distance_from_sun + 100, y = planet, label = paste0(planet, " - ", round(distance_from_sun)),
+                colour = planet),
+            show.legend = FALSE, family = "Goldman", size = 12, hjust = 0) +
+  scale_colour_manual(values = c("#97979f", "#e3bb76", "#8cb1de", "#c1440e",
+                                 "#e3dccc", "#e2bf7b", "#afdbf5", "#3e66f9")) +
+  xlim(0, 5500) +
+  annotate("text", x = 3000, y = 3, label = "Distances from the Sun", family = "Goldman", colour = "white",
+           size = 25) +
+  annotate("text", x = 3000, y = 1.5, label = "(in millions km)", family = "Goldman", colour = "white",
+           size = 25) +
+  theme_void() +
+  theme(plot.background = element_rect(fill = "black", colour = "black"),
+        panel.background = element_rect(fill = "black", colour = "black"),
+        plot.margin = margin(t = 20, b = 20))
+
+p <- p1 + p2 +
+  plot_layout(ncol = 1, widths = c(1, 1)) +
+  plot_annotation(
+    title = "Planets of the solar system",
+    caption = "Visualisation: Jonathan Kitt | Data source: www.devstronomy.com | #30DayChartChallenge 2022 | Day 7: physical",
+    theme = theme(plot.title = element_text(family = "Goldman", colour = "white", size = 120, hjust = 0.5,
+                                            margin = margin(t = 20, b = 20)),
+                  plot.background = element_rect(fill = "black", colour = "black"),
+                  plot.caption = element_text(colour = "white", hjust = 0.5, size = 25)))
 
 ggsave("2022/plots/07_physical.png", p, dpi = 320, width = 12, height = 6)
-  
-
-d1
-
-ggplot() +
-  geom_point(data = d1, aes(x = distance_from_sun, y = 0)) +
-  geom_point()
-
-ggplot() +
-  geom_circle(data = filter(d1, planet == "Mercury"),
-              aes(x0 = x.0, y0 = 0, r = diameter/2)) +
-  geom_circle(data = filter(d1, planet == "Venus"),
-              aes(x0 = x.0, y0 = 0, r = radius)) +
-  coord_fixed()
-
-  geom_circle(data = filter(d1, planet == "Venus"),
-              aes(x0 = 10000, y0 = 0, r = diameter/2)) +
-  geom_circle(data = filter(d1, planet == "Earth"),
-              aes(x0 = 30000, y0 = 0, r = diameter/2)) +
-  coord_fixed()
-
-+
-  geom_circle(data = filter(d1, planet == "Venus"),
-              aes(x0 = distance_from_sun*1e6, y0 = 0, r = diameter/2)) +
-  coord_fixed()
-  
-d1 %>% 
-  #filter(planet == "Sun") %>% 
-  ggplot() +
-  geom_circle(aes(x0 = -(diameter/2), y0 = distance_from_sun, r = diameter / 2),
-              fill = "yellow", colour = "yellow") +
-  coord_fixed()
-
-ggplot() +
-  geom_circle(aes(x0 = 0, y0 = 0, r = 10),
-              fill = "yellow", colour = "yellow") +
-  coord_fixed() +
-  xlim(0, 100) +
-  ylim(-20, 20)
-
-ggplot(d1, aes(x = distance_from_sun, y = 0)) +
-  geom_point(aes(size = diameter*100e4)) +
-  xlim(0, 6000)
-
-nuclear_weapons <- read_csv("2022/data/nuclear-warhead-stockpiles.csv")
-
-d1 <- nuclear_weapons %>% 
-  filter(!Entity %in% c("United States", "Russia", "United Kingdom", "France"))
-
-d2 <- nuclear_weapons %>% 
-  mutate(bin = cut_interval(nuclear_weapons_stockpile, length = 10000))
-
-ggplot(data = d2,
-       aes(x = Year, y = Entity, fill = bin)) +
-  geom_tile()
-
-boxplot(nuclear_weapons$Year ~ nuclear_weapons$Code)
-
-head(nuclear_weapons)
-
-ggplot(data = nuclear_weapons,
-       mapping = aes(x = Year, y = nuclear_weapons_stockpile,
-                     colour = Entity)) +
-  geom_line()
-
-# Data wrangling ----
-
-d1 <- tibble(
-  quartet_nb = rep(1:4, each = 11),
-  x = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8),
-  y = c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68,
-        9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74,
-        7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73,
-        6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89))
-
-# Create plot ----
-
-p1 <- ggplot(data = d1 %>% filter(quartet_nb == 1)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#ffa500") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-  
-p2 <- ggplot(data = d1 %>% filter(quartet_nb == 2)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#0099ff") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p3 <- ggplot(data = d1 %>% filter(quartet_nb == 3)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#009e73") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p4 <- ggplot(data = d1 %>% filter(quartet_nb == 4)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#b32db5") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p <- p1 + p2 + p3 + p4 +
-  plot_annotation(
-    title = "Anscombe's quartet",
-    subtitle = "Four datasets with nearly identical descriptive statistics but very different distributions",
-    caption = "Visualisation: Jonathan Kitt | Data source: www.cyclinglocations.com | #30DayChartChallenge 2022 | Day 3: historical",
-    theme = theme(plot.title = element_text(family = "Tangerine", colour = "black", size = 120, hjust = 0.5,
-                                            margin = margin(t = 20, b = 10)),
-                  plot.subtitle = element_text(family = "Tangerine", colour = "black", size = 75, hjust = 0.5,
-                                               margin = margin(b = 20)),
-                  plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-                  plot.caption = element_text(colour = "black", hjust = 0.5, size = 25)))
-
-ggsave("2022/plots/03_historical.png", p, dpi = 320, width = 12, height = 6)
