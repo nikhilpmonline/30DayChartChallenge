ba44258486d553db60e71a48df5a1d3d5ab33862
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-15 16:00:23 +0100

    day 9 ok

--- 2022/R/09_statistics.R
+++ 2022/R/09_statistics.R
@@ -4,167 +4,121 @@
 # Day 9 : Statistics
 # Last updated 2022-03-11
 
-# https://eliocamp.github.io/codigo-r/en/2021/09/contour-labels/
-# https://cmerow.github.io/RDataScience/05_Raster.html
-# https://www.youtube.com/watch?v=2lB2c_FTxpI
-
-# Testing ----
-
-library(raster)
-dem.raster <- getData("SRTM", lat = 46.0146, lon = 9.344197, download = TRUE)
-
-dem.raster <- crop(dem.raster, as(my_bbox_buff_25000.sf, 'Spatial'), snap='out')
-
-dem.m  <-  rasterToPoints(dem.raster)
-dem.df <-  data.frame(dem.m)
-colnames(dem.df) = c("lon", "lat", "alt")
-
-ggplot() +
-  geom_raster(data = dem.df, aes(lon, lat, fill = alt), alpha = .45) +
-  scale_fill_gradientn(colours = terrain.colors(100)) +
-  geom_sf(data = my_bbox_buff_2500.sf, fill = NA) +
-  coord_sf(xlim = c(st_bbox(my_bbox_buff_25000.sf)['xmin'], st_bbox(my_bbox_buff_25000.sf)['xmax']), 
-           ylim = c(st_bbox(my_bbox_buff_25000.sf)['ymin'], st_bbox(my_bbox_buff_25000.sf)['ymax'])) +
-  geom_polygon(data = my_world_map, 
-               aes(x=long, y = lat, group = group), fill = NA, colour = 'black') +
-  theme_bw()
-
-data("volcano")
-head(volcano)
-
-volcano_df <- volcano %>% 
-  reshape2::melt()
-
-ggplot(volcano_df, aes(Var1, Var2)) +
-  geom_contour(aes(z = value)) +
-  coord_equal()
-
-volcano_df %>% 
-  filter(value == max(value))
-
-d1 <- volcano_df %>% 
-  filter(Var2 == 31)
-
-ggplot(d1, aes(x = Var1, y = value)) +
-  geom_point()
-
-# Load packages ----
-
-library(tidyverse)
-library(showtext)
-# library(ggwaffle)
-# library(emojifont)
-library(patchwork)
-
 # Load fonts ----
 
-# font_add_google("Tangerine", "Tangerine")
-# showtext_auto()
-
-# Import data ----
-
-nuclear_weapons <- read_csv("2022/data/nuclear-warhead-stockpiles.csv")
-
-d1 <- nuclear_weapons %>% 
-  filter(!Entity %in% c("United States", "Russia", "United Kingdom", "France"))
-
-d2 <- nuclear_weapons %>% 
-  mutate(bin = cut_interval(nuclear_weapons_stockpile, length = 10000))
-
-ggplot(data = d2,
-       aes(x = Year, y = Entity, fill = bin)) +
-  geom_tile()
-
-boxplot(nuclear_weapons$Year ~ nuclear_weapons$Code)
-
-head(nuclear_weapons)
-
-ggplot(data = nuclear_weapons,
-       mapping = aes(x = Year, y = nuclear_weapons_stockpile,
-                     colour = Entity)) +
-  geom_line()
+font_add_google("Big Shoulders Inline Text", "Big Shoulders Inline Text")
+showtext_auto()
 
 # Data wrangling ----
 
-d1 <- tibble(
-  quartet_nb = rep(1:4, each = 11),
-  x = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8),
-  y = c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68,
-        9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74,
-        7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73,
-        6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89))
+binomial_data <- rbinom(n = 10000, size = 10, prob = 0.5)
+poisson_data <- rpois(n = 10000, lambda = 3)
+normal_data <- rnorm(n = 10000, mean = 0, sd = 1)
+lognorm_data <- rlnorm(n = 10000, meanlog = 0.2)
+beta_data <- rbeta(n = 10000, shape1 = 1, shape2 = 10)
+gamma_data <- rgamma(n = 10000, shape = 5)
 
 # Create plot ----
 
-p1 <- ggplot(data = d1 %>% filter(quartet_nb == 1)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#ffa500") +
+p1 <- ggplot() +
+  geom_bar(aes(x = binomial_data),
+           width = 0.2, fill = "white", colour = "white") +
+  annotate("text", x = 0, y = 2000, label = "Binomial",
+           family = "Big Shoulders Inline Text", size = 35,
+           colour = "#b589d6", hjust = 0) +
   theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
+  theme(panel.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        plot.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
         panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-  
-p2 <- ggplot(data = d1 %>% filter(quartet_nb == 2)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#0099ff") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
+        axis.title = element_blank(),
+        axis.text = element_blank())
 
-p3 <- ggplot(data = d1 %>% filter(quartet_nb == 3)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#009e73") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
+# p1 <- ggplot() +
+#   geom_bar(aes(x = binomial_data),
+#            width = 0.2, fill = "white", colour = "white") +
+#   xlab(label = "Binomial") +
+#   theme_minimal() +
+#   theme(panel.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+#         plot.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+#         panel.grid = element_blank(),
+#         axis.title.x = element_text(family = "Big Shoulders Inline Text",
+#                                     colour = "white", size = 50),
+#         axis.title.y = element_blank(),
+#         axis.text = element_blank())
 
-p4 <- ggplot(data = d1 %>% filter(quartet_nb == 4)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#b32db5") +
+p2 <- ggplot() +
+  geom_bar(aes(x = poisson_data),
+           width = 0.2, fill = "white", colour = "white") +
+  annotate("text", x = 5, y = 1500, label = "Poisson",
+           family = "Big Shoulders Inline Text", size = 35,
+           colour = "#b589d6", hjust = 0) +
   theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
+  theme(panel.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        plot.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
         panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
+        axis.title = element_blank(),
+        axis.text = element_blank())
 
-p <- p1 + p2 + p3 + p4 +
+p3 <- ggplot() +
+  geom_histogram(aes(x = normal_data),
+                 bins = 100, fill = "white", colour = "white") +
+  annotate("text", x = 0, y = 50, label = "Normal",
+           family = "Big Shoulders Inline Text", size = 35,
+           colour = "#b589d6", hjust = 0.5) +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        plot.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank())
+
+p4 <- ggplot() +
+  geom_histogram(aes(x = lognorm_data),
+                 bins = 100, fill = "white", colour = "white") +
+  annotate("text", x = 5, y = 1000, label = "Log Normal",
+           family = "Big Shoulders Inline Text", size = 35,
+           colour = "#b589d6", hjust = 0) +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        plot.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank())
+
+p5 <- ggplot() +
+  geom_histogram(aes(x = beta_data),
+                 bins = 100, fill = "white", colour = "white") +
+  annotate("text", x = 0.2, y = 200, label = "Beta",
+           family = "Big Shoulders Inline Text", size = 35,
+           colour = "#b589d6", hjust = 0) +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        plot.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank())
+
+p6 <- ggplot() +
+  geom_histogram(aes(x = gamma_data),
+                 bins = 100, fill = "white", colour = "white") +
+  annotate("text", x = 10, y = 200, label = "Gamma",
+           family = "Big Shoulders Inline Text", size = 35,
+           colour = "#b589d6", hjust = 0) +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        plot.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank())
+
+p <- p1 + p2 + p3 + p4 + p5 + p6 +
+  plot_layout(ncol = 3) +
   plot_annotation(
-    title = "Anscombe's quartet",
-    subtitle = "Four datasets with nearly identical descriptive statistics but very different distributions",
-    caption = "Visualisation: Jonathan Kitt | Data source: www.cyclinglocations.com | #30DayChartChallenge 2022 | Day 3: historical",
-    theme = theme(plot.title = element_text(family = "Tangerine", colour = "black", size = 120, hjust = 0.5,
+    title = "Statistical distributions",
+    caption = "Visualisation: Jonathan Kitt | #30DayChartChallenge 2022 | Day 9: statistical",
+    theme = theme(plot.title = element_text(family = "Big Shoulders Inline Text", colour = "white", size = 120, hjust = 0.5,
                                             margin = margin(t = 20, b = 10)),
-                  plot.subtitle = element_text(family = "Tangerine", colour = "black", size = 75, hjust = 0.5,
-                                               margin = margin(b = 20)),
-                  plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-                  plot.caption = element_text(colour = "black", hjust = 0.5, size = 25)))
+                  plot.background = element_rect(fill = "#804fb3", colour = "#804fb3"),
+                  plot.caption = element_text(colour = "white", hjust = 0.5, size = 25)))
 
-ggsave("2022/plots/03_historical.png", p, dpi = 320, width = 12, height = 6)
+ggsave("2022/plots/09_statistics.png", p, dpi = 320, width = 12, height = 6)
