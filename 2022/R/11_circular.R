ca40b6ba8ef3d14335cdb06dd406b9fa82c5f0d9
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-15 17:28:00 +0100

    update

--- 2022/R/11_circular.R
+++ 2022/R/11_circular.R
@@ -4,8 +4,90 @@
 # Day 11 : Circular
 # Last updated 2022-03-15
 
+# Load packages ----
 
-# Testing things ----
+library(tidyverse)
+library(showtext)
+# library(ggwaffle)
+# library(emojifont)
+library(patchwork)
+
+# Load fonts ----
+
+font_add_google("Fredericka the Great", "Fredericka")
+showtext_auto()
+
+
+# Data wrangling ----
+
+layer_1 <- tibble(
+  layer = 1,
+  x.min = seq(0, 63, 16),
+  x.max = seq(16, 64, 16),
+  y.min = 0,
+  y.max = 1,
+  base = c("U", "C", "A", "G"))
+
+layer_2 <- tibble(
+  layer = 2,
+  x.min = seq(0, 63, 4),
+  x.max = seq(4, 64, 4),
+  y.min = 1,
+  y.max = 2,
+  base = rep(c("U", "C", "A", "G"), times = 4))
+
+layer_3 <- tibble(
+  layer = 3,
+  x.min = seq(0, 63, 1),
+  x.max = seq(1, 64, 1),
+  y.min = 2,
+  y.max = 3,
+  base = rep(c("U", "C", "A", "G"), times = 16))
+
+prot <- tibble(
+  layer = 4,
+  x.min = c(0, 2, 4, 8, 10, 12, 14, 15, 16, 20, 24, 26, 28, 32, 35, 36, 40, 42, 44, 46, 48, 52, 56, 58, 60),
+  x.max = c(2, 4, 8, 10, 12, 14, 15, 16, 20, 24, 26, 28, 32, 35, 36, 40, 42, 44, 46, 48, 52, 56, 58, 60, 64),
+  y.min = 3,
+  y.max = 4,
+  prot = c("PHE", "LEU", "SER", "TYR", "STOP", "LYS", "STOP", "TRP", "LEU", "PRO", "HIS", "GLN", "ARG",
+           "ILE", "MET", "THR", "ASN", "LYS", "SER", "ARG", "VAL", "ALA", "ASP", "GLU", "GLY"))
+
+codons <- rbind(layer_1, layer_2, layer_3)
+
+# Create plot ----
+
+p <- ggplot() +
+  geom_rect(data = codons,
+            aes(xmin = x.min, xmax = x.max, ymin = y.min, ymax = y.max,
+                fill = base),
+            colour = "white", show.legend = FALSE) +
+  scale_fill_manual(values = c("#7f58af", "#64c5eb", "#e84d8a", "#feb326")) +
+  geom_rect(data = prot,
+            aes(xmin = x.min, xmax = x.max, ymin = y.min, ymax = y.max),
+            colour = "white", fill = "#0b0742", show.legend = FALSE) +
+  geom_text(data = codons,
+            aes(x = x.min + (x.max - x.min)/2,
+                y = y.min + (y.max - y.min) / 2,
+                label = base), 
+            colour = "white", size = 10) +
+  geom_text(data = prot,
+            aes(x = x.min + (x.max - x.min)/2,
+                y = y.min + (y.max - y.min) / 2,
+                label = prot), 
+            colour = "white", size = 6) +
+  coord_polar(theta = "x", start = 0) +
+  ggtitle("Genetic code") +
+  labs(caption = "Visualisation: Jonathan Kitt | Data source: Wikipedia| #30DayChartChallenge 2022 | Day 11: circular)") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#0b0742", colour = "#0b0742"),
+        plot.background = element_rect(fill = "#0b0742", colour = "#0b0742"),
+        plot.title = element_text(family = "Fredericka", colour = "white", size = 100, hjust = 0.5,
+                                  margin = margin(t = 20)),
+        plot.caption = element_text(colour = "white", size = 25, hjust = 0.5))
+
+
+ggsave("2022/plots/11_circular.png", p, dpi = 320, width = 12, height = 6)
 
 d1 <- tibble(
   first_base = rep(c("U", "C", "A", "G"), each = 16),
@@ -55,18 +137,7 @@ ggplot() +
             colour = "white") +
   coord_polar(theta = "x", start = 0)
 
-# Load packages ----
 
-library(tidyverse)
-library(showtext)
-# library(ggwaffle)
-# library(emojifont)
-library(patchwork)
-
-# Load fonts ----
-
-# font_add_google("Tangerine", "Tangerine")
-# showtext_auto()
 
 # Import data ----
 
