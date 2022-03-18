e3c767100dc9b2c0110a24d5910680c3da71af50
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-18 14:33:33 +0100

    plot circular ok

--- 2022/R/11_circular.R
+++ 2022/R/11_circular.R
@@ -2,7 +2,7 @@
 # 2022
 # Category : Distributions
 # Day 11 : Circular
-# Last updated 2022-03-15
+# Last updated 2022-03-18
 
 # Load packages ----
 
@@ -55,6 +55,17 @@ prot <- tibble(
 
 codons <- rbind(layer_1, layer_2, layer_3)
 
+d1 <- tibble(
+  quartet_nb = rep(1:4, each = 11),
+  x = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
+        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
+        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
+        8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8),
+  y = c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68,
+        9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74,
+        7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73,
+        6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89))
+
 # Create plot ----
 
 p <- ggplot() +
@@ -70,7 +81,7 @@ p <- ggplot() +
             aes(x = x.min + (x.max - x.min)/2,
                 y = y.min + (y.max - y.min) / 2,
                 label = base), 
-            colour = "white", size = 10) +
+            colour = "white", size = 8) +
   geom_text(data = prot,
             aes(x = x.min + (x.max - x.min)/2,
                 y = y.min + (y.max - y.min) / 2,
@@ -78,7 +89,7 @@ p <- ggplot() +
             colour = "white", size = 6) +
   coord_polar(theta = "x", start = 0) +
   ggtitle("Genetic code") +
-  labs(caption = "Visualisation: Jonathan Kitt | Data source: Wikipedia| #30DayChartChallenge 2022 | Day 11: circular)") +
+  labs(caption = "Visualisation: Jonathan Kitt | Data source: Wikipedia| #30DayChartChallenge 2022 | Day 11: circular") +
   theme_void() +
   theme(panel.background = element_rect(fill = "#0b0742", colour = "#0b0742"),
         plot.background = element_rect(fill = "#0b0742", colour = "#0b0742"),
@@ -87,162 +98,5 @@ p <- ggplot() +
         plot.caption = element_text(colour = "white", size = 25, hjust = 0.5))
 
 
+
 ggsave("2022/plots/11_circular.png", p, dpi = 320, width = 12, height = 6)
-
-d1 <- tibble(
-  first_base = rep(c("U", "C", "A", "G"), each = 16),
-  second_base = rep(rep(c("U", "C", "A", "G"), each = 4), times = 4),
-  third_base = rep(c("U", "C", "A", "G"), times = 16))
-
-layer_2 <- tibble(
-  layer = 2,
-  x.min = seq(0, 63, 16),
-  x.max = seq(16, 64, 16),
-  y.min = 0,
-  y.max = 1)
-
-layer_1 <- tibble(
-  layer = 1,
-  x.min = seq(0, 63, 4),
-  x.max = seq(4, 64, 4),
-  y.min = 1,
-  y.max = 2)
-
-layer_3 <- tibble(
-  layer = 3,
-  x.min = seq(0, 63, 1),
-  x.max = seq(1, 64, 1),
-  y.min = 2,
-  y.max = 3)
-
-
-layers <- rbind(layer_1, layer_2, layer_3)
-
-
-
-d2 <- tibble(
-  layer = 1,
-  x.min = seq(0, 63, 16),
-  x.max = seq(16, 64, 16),
-  y.min = 0,
-  y.max = 1
-)
-
-d2
-
-ggplot() +
-  geom_rect(data = layers,
-            aes(xmin = x.min, xmax = x.max, ymin = y.min, ymax = y.max,
-                fill = layer),
-            colour = "white") +
-  coord_polar(theta = "x", start = 0)
-
-
-
-# Import data ----
-
-nuclear_weapons <- read_csv("2022/data/nuclear-warhead-stockpiles.csv")
-
-d1 <- nuclear_weapons %>% 
-  filter(!Entity %in% c("United States", "Russia", "United Kingdom", "France"))
-
-d2 <- nuclear_weapons %>% 
-  mutate(bin = cut_interval(nuclear_weapons_stockpile, length = 10000))
-
-ggplot(data = d2,
-       aes(x = Year, y = Entity, fill = bin)) +
-  geom_tile()
-
-boxplot(nuclear_weapons$Year ~ nuclear_weapons$Code)
-
-head(nuclear_weapons)
-
-ggplot(data = nuclear_weapons,
-       mapping = aes(x = Year, y = nuclear_weapons_stockpile,
-                     colour = Entity)) +
-  geom_line()
-
-# Data wrangling ----
-
-d1 <- tibble(
-  quartet_nb = rep(1:4, each = 11),
-  x = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8),
-  y = c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68,
-        9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74,
-        7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73,
-        6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89))
-
-# Create plot ----
-
-p1 <- ggplot(data = d1 %>% filter(quartet_nb == 1)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#ffa500") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-  
-p2 <- ggplot(data = d1 %>% filter(quartet_nb == 2)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#0099ff") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p3 <- ggplot(data = d1 %>% filter(quartet_nb == 3)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#009e73") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p4 <- ggplot(data = d1 %>% filter(quartet_nb == 4)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#b32db5") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p <- p1 + p2 + p3 + p4 +
-  plot_annotation(
-    title = "Anscombe's quartet",
-    subtitle = "Four datasets with nearly identical descriptive statistics but very different distributions",
-    caption = "Visualisation: Jonathan Kitt | Data source: www.cyclinglocations.com | #30DayChartChallenge 2022 | Day 3: historical",
-    theme = theme(plot.title = element_text(family = "Tangerine", colour = "black", size = 120, hjust = 0.5,
-                                            margin = margin(t = 20, b = 10)),
-                  plot.subtitle = element_text(family = "Tangerine", colour = "black", size = 75, hjust = 0.5,
-                                               margin = margin(b = 20)),
-                  plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-                  plot.caption = element_text(colour = "black", hjust = 0.5, size = 25)))
-
-ggsave("2022/plots/03_historical.png", p, dpi = 320, width = 12, height = 6)
