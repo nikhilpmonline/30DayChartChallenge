5e5f6403d1a8040d1076dc8bea2f23a9b0510301
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-23 11:28:52 +0100

    update

--- 2022/R/08_mountains.R
+++ 2022/R/08_mountains.R
@@ -2,145 +2,31 @@
 # 2022
 # Category : Distributions
 # Day 8 : Mountains
-# Last updated 2022-03-11
+# Last updated 2022-03-23
 
-# https://cran.r-project.org/web/packages/ggridges/vignettes/gallery.html
-# https://katherinemwood.github.io/post/joy/
-# https://www.anychart.com/blog/2019/03/01/new-great-data-visualization-examples-elevation-energy-misspelling-jobs-dataviz-weekly/
-# https://www.thingsmadethinkable.com/item/earth_elevation_cross_sections_along_parallels.php
-
-# Testing things ----
-
-# https://gis.stackexchange.com/questions/359822/adding-elevation-point-data-and-lines-to-topography-plot-using-ggplot2
-
-library(tidyverse)
-library(raster)
-library(scales)
-library(rgeos)
-
-#get elevation data
-dem1<- getData("SRTM",lat=34.5,lon=33)
-dem2<- getData("SRTM",lat=34.5,lon=36)
-dem <- merge(dem1,dem2)
+# https://education.rstudio.com/blog/2020/07/palmerpenguins-cran/
 
 # Load packages ----
 
-
+library(palmerpenguins)
+library(ggridges)
 library(tidyverse)
-library(showtext)
-# library(ggwaffle)
-# library(emojifont)
-library(patchwork)
-
-# Load fonts ----
-
-# font_add_google("Tangerine", "Tangerine")
-# showtext_auto()
 
 # Import data ----
 
-nuclear_weapons <- read_csv("2022/data/nuclear-warhead-stockpiles.csv")
-
-d1 <- nuclear_weapons %>% 
-  filter(!Entity %in% c("United States", "Russia", "United Kingdom", "France"))
-
-d2 <- nuclear_weapons %>% 
-  mutate(bin = cut_interval(nuclear_weapons_stockpile, length = 10000))
-
-ggplot(data = d2,
-       aes(x = Year, y = Entity, fill = bin)) +
-  geom_tile()
-
-boxplot(nuclear_weapons$Year ~ nuclear_weapons$Code)
-
-head(nuclear_weapons)
-
-ggplot(data = nuclear_weapons,
-       mapping = aes(x = Year, y = nuclear_weapons_stockpile,
-                     colour = Entity)) +
-  geom_line()
+penguins <- palmerpenguins::penguins
 
 # Data wrangling ----
 
-d1 <- tibble(
-  quartet_nb = rep(1:4, each = 11),
-  x = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8),
-  y = c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68,
-        9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74,
-        7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73,
-        6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89))
+d1 <- penguins %>% 
+  mutate(ratio = bill_length_mm / bill_depth_mm)
 
 # Create plot ----
 
-p1 <- ggplot(data = d1 %>% filter(quartet_nb == 1)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#ffa500") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-  
-p2 <- ggplot(data = d1 %>% filter(quartet_nb == 2)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#0099ff") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
+p <- ggplot(d1, aes(x = ratio, fill = species)) +
+  geom_density_ridges(aes(y = species)) +
+  theme_minimal()
 
-p3 <- ggplot(data = d1 %>% filter(quartet_nb == 3)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#009e73") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
+# Save plot ----
 
-p4 <- ggplot(data = d1 %>% filter(quartet_nb == 4)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#b32db5") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p <- p1 + p2 + p3 + p4 +
-  plot_annotation(
-    title = "Anscombe's quartet",
-    subtitle = "Four datasets with nearly identical descriptive statistics but very different distributions",
-    caption = "Visualisation: Jonathan Kitt | Data source: www.cyclinglocations.com | #30DayChartChallenge 2022 | Day 3: historical",
-    theme = theme(plot.title = element_text(family = "Tangerine", colour = "black", size = 120, hjust = 0.5,
-                                            margin = margin(t = 20, b = 10)),
-                  plot.subtitle = element_text(family = "Tangerine", colour = "black", size = 75, hjust = 0.5,
-                                               margin = margin(b = 20)),
-                  plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-                  plot.caption = element_text(colour = "black", hjust = 0.5, size = 25)))
-
-ggsave("2022/plots/03_historical.png", p, dpi = 320, width = 12, height = 6)
+ggsave("2022/plots/08_mountains.png", p, dpi = 320, width = 12, height = 6)
