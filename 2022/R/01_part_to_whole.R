63aaad3f2ae44d584db54fd9ea1bdefb30f88aed
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-23 09:32:53 +0100

    update

--- 2022/R/01_part_to_whole.R
+++ 2022/R/01_part_to_whole.R
@@ -9,15 +9,12 @@
 library(tidyverse)
 library(showtext)
 library(patchwork)
+library(ggtext)
 
 # Load fonts ----
 
 font_add_google("Lobster", "Lobster")
 showtext_auto()
-
-# Import data ----
-
-munros <- munro::munros
 
 # Data wrangling ---- 
 
@@ -27,72 +24,44 @@ d1 <- tibble(
   mutate(ymin = c(0, max(ratio)),
          ymax = c(max(ratio), 1))
 
-d1
-
 # Create plot ----
 
 p1 <- ggplot(data = d1,
        aes(x = 6, y = ratio, fill = status)) +
   geom_bar(stat = "identity",
            show.legend = FALSE) +
-  #scale_fill_manual(values = c("#04aed9", "#c5e0f5")) +
-  scale_fill_manual(values = c("#003c54", "white")) +
+  scale_fill_manual(values = c("#00a1e0", "white")) +
   coord_polar(theta = "y", start = 0) +
   xlim(c(0.025, 8)) +
-  annotate("text", x = 0.025, y = 0.5, label = "80 %", colour = "white", size = 60, family = "Lobster") +
+  annotate("text", x = 0.025, y = 0.5, label = "20 %", colour = "#00a1e0", size = 60, family = "Lobster") +
   theme_void() +
   theme(panel.background = element_rect(fill = "#003c54", colour = "#003c54"),
         plot.background = element_rect(fill = "#003c54", colour = "#003c54"))
 
-ggsave("2022/plots/01_part_to_whole_1.png", p, dpi = 320, width = 12, height = 6)
-
-d1 <- munros %>% 
-  mutate(unique_name = paste(1:nrow(.), name, sep = " - ")) %>% 
-  select(unique_name, county) %>% 
-  separate_rows(county, sep = ",") %>% 
-  mutate(county = factor(str_trim(county))) %>% 
-  count(county)
-
-highland_munros <- tibble(
-  county = c("Highland", "Other"),
-  n = c(d1$n[d1$county == "Highland"],
-        nrow(munros) - d1$n[d1$county == "Highland"])) %>% 
-  mutate(ratio = n / sum(n)) %>% 
-  mutate(ymin = c(0, max(ratio)),
-         ymax = c(max(ratio), 1))
-
-# Create plot ----
-
-p1 <- ggplot() +
-  annotate("text", x = -0.5, y = 1, label = "Munros are mountains in Scotland above 3,000 feet",
-           colour = "white", family = "Lobster", size = 20) +
-  annotate("text", x = 0.5, y = -1, label = "Out of 282 Munros, 186 are located in the Highlands",
-           colour = "white", family = "Lobster", size = 20) +
-  xlim(-15, 15) +
+p2 <- ggplot() +
+  annotate("text", x = 0, y = 1, label = "More than", 
+           colour = "#00a1e0", family = "Lobster", size = 25, hjust = 0) +
+  annotate("text", x = 20, y = 1, label = "80%", 
+           colour = "white", family = "Lobster", size = 25, hjust = 0) +
+  annotate("text", x = 30, y = 1, label = "of our ocean is", 
+           colour = "#00a1e0", family = "Lobster", size = 25, hjust = 0) +
+  annotate("text", x = 0, y = -1, label = "unmapped, unobserved, and unexplored.", 
+           colour = "white", family = "Lobster", size = 25, hjust = 0) +
+  xlim(0, 100) +
   ylim(-5, 5) +
   theme_void() +
-  theme(panel.background = element_rect(fill = "#041f32", colour = "#041f32"),
-        plot.background = element_rect(fill = "#041f32", colour = "#041f32"))
-
-p2 <- ggplot(data = highland_munros,
-             aes(x = 6, y = ratio, fill = county)) +
-  geom_bar(stat = "identity",
-           show.legend = FALSE) +
-  #scale_fill_manual(values = c("#04aed9", "#c5e0f5")) +
-  scale_fill_manual(values = c("#04aed9", "white")) +
-  coord_polar(theta = "y", start = 0) +
-  xlim(c(0.025, 8)) +
-  theme_void() +
-  theme(panel.background = element_rect(fill = "#041f32", colour = "#041f32"),
-        plot.background = element_rect(fill = "#041f32", colour = "#041f32"))
+  theme(panel.background = element_rect(fill = "#003c54", colour = "#003c54"),
+        plot.background = element_rect(fill = "#003c54", colour = "#003c54"))
 
 p <- p1 + p2 +
   plot_annotation(
-    title = "How many Munros are located in the Highlands?",
-    caption = "Visualisation: Jonathan Kitt | Data source: munro package | #30DayChartChallenge 2022 | Day 1: part-to-whole",
-    theme = theme(plot.title = element_text(family = "Lobster", colour = "white", size = 100, hjust = 0.5,
+    title = "How much of the ocean have we explored ?",
+    caption = "Visualisation: Jonathan Kitt | Data source: National Ocean Service | #30DayChartChallenge 2022 | Day 1: part-to-whole",
+    theme = theme(plot.title = element_text(family = "Lobster", colour = "#00a1e0", size = 100, hjust = 0.5,
                                             margin = margin(t = 40)),
-                  plot.background = element_rect(fill = "#041f32", colour = "#041f32"),
+                  plot.background = element_rect(fill = "#003c54", colour = "#003c54"),
                   plot.caption = element_text(colour = "white", hjust = 0.5, size = 25)))
 
+# Save plot ----
+  
 ggsave("2022/plots/01_part_to_whole.png", p, dpi = 320, width = 12, height = 6)
