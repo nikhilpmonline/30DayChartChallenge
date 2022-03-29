aea5cc96e81d960fe93a5c0f08ba26c565f8e657
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-03-29 12:08:26 +0200

    update

--- 2022/R/11_circular.R
+++ 2022/R/11_circular.R
@@ -2,7 +2,7 @@
 # 2022
 # Category : Distributions
 # Day 11 : Circular
-# Last updated 2022-03-28
+# Last updated 2022-03-29
 
 # Sources : 
 # https://en.wikipedia.org/wiki/DNA_and_RNA_codon_tables#Translation_table_1
@@ -17,6 +17,7 @@ library(tidyverse)
 # Load fonts ----
 
 font_add_google("Fredericka the Great", "Fredericka")
+font_add_google("Gluten", "Gluten")
 showtext_auto()
 
 
@@ -60,14 +61,16 @@ aa <- tibble(
              "Valine", "Alanine", "Aspartic acid", "Glutaminc acid", "Glycine"))
 
 aa_names <- tibble(
-  code = c("A", "G", "I", "L", "P", "V", "P", "W", "Y", "D", "E", "R", "H", "K", "S", "T", "C", "M", "N", "Q"),
+  code = c("A", "G", "I", "L", "P", "V", "F", "W", "Y", "D", "E", "R", "H", "K", "S", "T", "C", "M", "N", "Q"),
   name = c("ALANINE", "GLYCINE", "ISOLEUCINE", "LEUCINE", "PROLINE", "VALINE", "PHENYLALANINE",
            "TRYPTOPHAN", "TYROSINE", "ASPARTIC ACID", "GLUTAMIC ACID", "ARGININE", "HISTIDINE",
            "LYSINE", "SERINE", "THREONINE", "CYSTEINE", "METHIONINE", "ASPARAGINE", "GLUTAMINE"),
   type = c(rep("ALIPHATIC", 6), rep("AROMATIC", 3), rep("ACIDIC", 2), rep("BASIC", 3), 
            rep("HYDROXYLIC", 2), rep("SULFUR-CONTAINING", 2), rep("AMIDIC", 2)),
-  y.pos = rep(c(11:1, 11:3)),
-  x.pos = c(rep(1, times = 11), rep(2, times = 9)))
+  y.pos = c(9:4, 9:5, 9:5, 9:6),
+  x.pos = c(rep(1, times = 6), rep(5, times = 5), rep(10, times = 5), rep(14, times = 4))) %>% 
+  mutate(type = factor(type, levels = c("ALIPHATIC", "AROMATIC", "ACIDIC", "BASIC",
+                                        "HYDROXYLIC", "SULFUR-CONTAINING", "AMIDIC")))
 
 codons <- rbind(layer_1, layer_2, layer_3)
 
@@ -85,43 +88,78 @@ p1 <- ggplot() +
   geom_text(data = codons,
             aes(x = x.min + (x.max - x.min)/2,
                 y = y.min + (y.max - y.min) / 2,
-                label = base), 
+                label = base, family = "Gluten"), 
             colour = "white", size = 8) +
   geom_text(data = aa,
             aes(x = x.min + (x.max - x.min)/2,
                 y = y.min + (y.max - y.min) / 2,
                 label = code_1_letter), 
-            colour = "white", size = 8) +
+            colour = "white", size = 8, family = "Gluten") +
   coord_polar(theta = "x", start = 0) +
-  # ggtitle("Genetic code") +
-  # labs(caption = "Visualisation: Jonathan Kitt | Data source: Wikipedia| #30DayChartChallenge 2022 | Day 11: circular") +
   theme_void() +
   theme(panel.background = element_rect(fill = "#0b0742", colour = "#0b0742"),
-        plot.background = element_rect(fill = "#0b0742", colour = "#0b0742"),
-        plot.title = element_text(family = "Fredericka", colour = "white", size = 100, hjust = 0.5,
-                                  margin = margin(t = 20)),
-        plot.caption = element_text(colour = "white", size = 25, hjust = 0.5))
+        plot.background = element_rect(fill = "#0b0742", colour = "#0b0742"))
 
-ggplot(data = aa_names) +
-  geom_point(aes(x = x.pos, y = y.pos, colour = type),
-             size = 8, shape = 19) +
+p2 <- ggplot(data = aa_names) +
+  geom_point(aes(x = x.pos, y = y.pos, colour = type, stroke = 1.5),
+             size = 8,  shape = 21, show.legend = FALSE) +
+  scale_colour_manual(values = c("#d11141", "#00b159", "#f37735", "#00aedb",
+                                 "#8c8c8c", "#ffc425", "#cccccc")) +
   geom_text(aes(x = x.pos, y = y.pos,
-                label = code)) +
-  geom_text(aes(x = x.pos + 0.2, y = y.pos,
-                label = name), hjust = 0) +
-  xlim(0, 3) +
-  theme(legend.position = "bottom",
-        legend.direction = "horizontal")
+                label = code), colour = "white",
+            size = 10) +
+  geom_text(aes(x = x.pos + 0.75, y = y.pos,
+                label = name), hjust = 0, size = 10, colour = "white",
+            family = "Gluten") +
+  annotate("text", family = "Gluten", colour = "white", x = 0, y = 2, size = 15, hjust = 0,
+           label = "The 20 amino-acids encoded by the human genetic code") +
+  annotate("text", family = "Gluten", colour = "white", x = 0, y = 1.5, size = 15, hjust = 0,
+           label = "are either") +
+  # annotate("text", family = "Gluten", colour = "#d11141", x = 2.75, y = 1.5, size = 15, hjust = 0,
+  #          label = "aliphatic") +
+  # annotate("text", family = "Gluten", colour = "white", x = 5.1, y = 1.5, size = 15, hjust = 0,
+  #          label = ", ") +
+  # annotate("text", family = "Gluten", colour = "#00b159", x = 5.5, y = 1.5, size = 15, hjust = 0,
+  #          label = "aromatic") +
+  # annotate("text", family = "Gluten", colour = "white", x = 7.95, y = 1.5, size = 15, hjust = 0,
+  #          label = ", ") +
+  # annotate("text", family = "Gluten", colour = "#f37735", x = 9.35, y = 1.5, size = 15, hjust = 0,
+  #          label = "acidic") +
+  # annotate("text", family = "Gluten", colour = "white", x = 11.15, y = 1.5, size = 15, hjust = 0,
+  #          label = ", ") +
+  # annotate("text", family = "Gluten", colour = "#00aedb", x = 11.55, y = 1.5, size = 15, hjust = 0,
+  #          label = "basic") +
+  # annotate("text", family = "Gluten", colour = "white", x = 13.15, y = 1.5, size = 15, hjust = 0,
+  #          label = ", ") +
+  # annotate("text", family = "Gluten", colour = "#8c8c8c", x = 0, y = 1, size = 15, hjust = 0,
+  #          label = "hydroxylic") +
+  # annotate("text", family = "Gluten", colour = "white", x = 3.2, y = 1, size = 15, hjust = 0,
+  #          label = ", ") +
+  # annotate("text", family = "Gluten", colour = "#ffc425", x = 3.6, y = 1, size = 15, hjust = 0,
+  #          label = "sulfur-containing") +
+  # annotate("text", family = "Gluten", colour = "white", x = 9.2, y = 1, size = 15, hjust = 0,
+  #          label = "or ") +
+  # annotate("text", family = "Gluten", colour = "#cccccc", x = 10.1, y = 1, size = 15, hjust = 0,
+  #          label = "amidic") +
+  # annotate("text", family = "Gluten", colour = "white", x = 12, y = 1, size = 15, hjust = 0,
+  #          label = ".") +
+  xlim(0, 18) +
+  ylim(0, 12) +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#0b0742", colour = "#0b0742"),
+        plot.background = element_rect(fill = "#0b0742", colour = "#0b0742"))
 
-  geom_text(aes(x = 0.5, y = y.pos, label = code)) +
-+
-  geom_text(aes(x = 0.5, y = y.pos, label = code)) +
-  geom_text(aes(x = 1, y = y.pos, label = name), hjust = 0) +
-  xlim(0, 10)
-  
 
-p <- p1 + p2
+p <- p1 + p2 +
+  plot_annotation(
+    title = "Genetic code",
+    caption = "Visualisation: Jonathan Kitt | Data source: Wikipedia| #30DayChartChallenge 2022 | Day 11: circular",
+    theme = theme(plot.title = element_text(family = "Fredericka", colour = "white", size = 120, hjust = 0.5,
+                                            margin = margin(t = 20)),
+                  plot.background = element_rect(fill = "#0b0742", colour = "#0b0742"),
+                  plot.caption = element_text(colour = "white", hjust = 0.5, size = 25)))
+
+
+# Save plot ----
 
 ggsave("2022/plots/work_in_progress/11_circular.png", p, dpi = 320, width = 12, height = 6)
-
-
