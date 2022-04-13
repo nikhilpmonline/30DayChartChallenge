98727a8339e087fb632f7fa277ffc7c066b406dd
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-04-13 07:19:05 +0200

    day 13 plot ok

--- 2022/R/13_correlation.R
+++ 2022/R/13_correlation.R
@@ -2,7 +2,7 @@
 # 2022
 # Category : Relationships
 # Day 13 : Correlation
-# Last updated 2022-03-14
+# Last updated 2022-04-13
 
 # https://www.tylervigen.com/spurious-correlations
 
@@ -91,4 +91,4 @@ p <- ggplot() +
 
 # Save plot ----
 
-ggsave("2022/plots/finished/13_correlation.png", p, dpi = 320, width = 12, height = 6)
+ggsave("2022/plots/13_correlation.png", p, dpi = 320, width = 12, height = 6)
