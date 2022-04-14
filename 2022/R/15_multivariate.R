ec66b1484dc75c7bf2cf45b70c93ffb5e71877ea
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-04-14 11:51:03 +0200

    day 15 plot ok

--- 2022/R/15_multivariate.R
+++ 2022/R/15_multivariate.R
@@ -2,16 +2,17 @@
 # 2022
 # Category : Relationships
 # Day 15 - multivariate
-# Last updated 2022-04-13
+# Last updated 2022-04-15
 
 # https://allisonhorst.github.io/palmerpenguins/
 
 # Load packages ----
 
 library(GGally)
+library(ggtext)
 library(palmerpenguins)
-library(tidyverse)
 library(showtext)
+library(tidyverse)
 
 # Load fonts ----
 
@@ -28,244 +29,34 @@ d1 <- penguins %>%
   select(species, "Bill length (mm)" = bill_length_mm,
          "Bill depth (mm)" = bill_depth_mm, "Flipper length (mm)" = flipper_length_mm)
 
+# Create plot ----
+
 p <- ggpairs(data = d1, columns = 2:4,
         aes(colour = species),
         upper = "blank") +
+  labs(
+    title = "**Palmer penguins**",
+    subtitle = "Comparing three penguin species from the Palmer Archipelago in Antarctica : 
+    <span style='color:darkorange;'>**Adelie**</span>, 
+    <span style='color:purple;'>Chinstrap</span>, and
+    <span style='color:cyan4;'>Gentoo</span>.
+    </span>") +
   scale_colour_manual(values = c("darkorange", "purple", "cyan4")) +
   scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
   theme(panel.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
         plot.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
+        plot.title = element_markdown(family = "Righteous", size = 75, colour = "#6675bd",
+                                      margin = margin(t = 10, b = 5)),
+        plot.subtitle = element_markdown(family = "Righteous", size = 50, colour = "#6675bd",
+                                      margin = margin(t = 5, b = 20)),
         panel.grid.major = element_line(colour = "white"),
         panel.grid.minor = element_blank(),
-        axis.text = element_text(colour = "#6675bd"),
-        axis.title = element_text(family = "Righteous", colour = "#6675bd"),
+        axis.text = element_text(family = "Righteous", colour = "#6675bd", size = 20),
         axis.ticks = element_blank(),
-        strip.background = element_rect(fill = "#92dff3"),
-        strip.text = element_text(colour = "#6675bd", size = 25, family = "Righteous"))
-
-ggsave("2022/plots/work_in_progress/15_multivariate.png", p, dpi = 320, width = 12, height = 6)
-
-
-
-# Create plot ----
-
-p1 <- ggplot(data = d1,
-       aes(x = species, y = bill_length_mm, colour = species)) +
-  geom_violin(aes(fill = species),
-              size = 1, alpha = 0.5,
-              show.legend = FALSE) +
-  geom_boxplot(outlier.alpha = 0, coef = 0, width = 0.2,
-               show.legend = FALSE) +
-  scale_colour_manual(values = c("darkorange", "purple", "cyan4")) +
-  scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
-  coord_flip() +
-  ggtitle("Bill length (mm)") +
-  theme_minimal() +
-  theme(panel.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
-        plot.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),,
-        axis.title = element_blank(),
-        axis.text.y = element_blank(),
-        plot.title = element_text(family = "Righteous"),
-        panel.grid.minor = element_blank(),
-        panel.grid.major.y = element_blank())
-  
-p2 <- ggplot(data = d1,
-             aes(x = species, y = bill_depth_mm, colour = species)) +
-  geom_violin(aes(fill = species),
-              size = 1, alpha = 0.5,
-              show.legend = FALSE) +
-  geom_boxplot(outlier.alpha = 0, coef = 0, width = 0.2,
-               show.legend = FALSE) +
-  scale_colour_manual(values = c("darkorange", "purple", "cyan4")) +
-  scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
-  coord_flip() +
-  ggtitle("Bill depth (mm)") +
-  theme_minimal() +
-  theme(panel.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
-        plot.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
-        axis.title = element_blank(),
-        axis.text.y = element_blank(),
-        plot.title = element_text(family = "Righteous"),
-        panel.grid.minor = element_blank(),
-        panel.grid.major.y = element_blank())
-
-p3 <- ggplot(data = d1,
-             aes(x = species, y = flipper_length_mm, colour = species)) +
-  geom_violin(aes(fill = species),
-              size = 1, alpha = 0.5,
-              show.legend = FALSE) +
-  geom_boxplot(outlier.alpha = 0, coef = 0, width = 0.2,
-               show.legend = FALSE) +
-  scale_colour_manual(values = c("darkorange", "purple", "cyan4")) +
-  scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
-  coord_flip() +
-  ggtitle("Flipper length (mm)") +
-  theme_minimal() +
-  theme(panel.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
-        plot.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
-        axis.title = element_blank(),
-        axis.text.y = element_blank(),
-        plot.title = element_text(family = "Righteous"),
-        panel.grid.minor = element_blank(),
-        panel.grid.major.y = element_blank())
-
-p4 <- ggplot(data = d1,
-             aes(x = species, y = length_depth_ratio, colour = species)) +
-  geom_violin(aes(fill = species),
-              size = 1, alpha = 0.5,
-              show.legend = FALSE) +
-  geom_boxplot(outlier.alpha = 0, coef = 0, width = 0.2,
-               show.legend = FALSE) +
-  scale_colour_manual(values = c("darkorange", "purple", "cyan4")) +
-  scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
-  coord_flip() +
-  ggtitle("Bill length to bill depth ratio") +
-  theme_minimal() +
-  theme(panel.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
-        plot.background = element_rect(fill = "#dbf3fa", colour = "#dbf3fa"),
-        axis.title = element_blank(),
-        axis.text.y = element_blank(),
-        plot.title = element_text(family = "Righteous"),
-        panel.grid.minor = element_blank(),
-        panel.grid.major.y = element_blank())
-
-p <- p1 + p2 + p3 + p4
+        strip.background = element_rect(fill = "#6675bd"),
+        # strip.background = element_rect(fill = "#92dff3"),
+        strip.text = element_text(colour = "white", size = 25, family = "Righteous"))
 
 # Save plot ----
 
 ggsave("2022/plots/work_in_progress/15_multivariate.png", p, dpi = 320, width = 12, height = 6)
-
-g + geom_boxplot()
-
-g + geom_point(alpha = 0.1)
-
-g + geom_jitter(width = 0.3, alpha = 0.5)
-
-g + geom_violin(aes(fill = species), size = 1, alpha = 0.5) +
-  geom_boxplot(outlier.alpha = 0, coef = 0, colour = "gray40", width = 0.2)
-+
-  geom_sina(alpha = 0.25) +
-  coord_flip()
-
-ggpairs(data = d1, columns = 2:5,
-        aes(colour = species),
-        upper = "blank") +
-  scale_colour_manual(values = c("darkorange", "purple", "cyan4")) +
-  scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
-  theme(panel.background = element_rect(fill = "#161b33", colour = "#161b33"),
-        plot.background = element_rect(fill = "#161b33", colour = "#161b33"),
-        panel.grid.major = element_line(colour = "#6675bd"),
-        panel.grid.minor = element_blank(),
-        axis.text = element_text(colour = "#6675bd"),
-        axis.title = element_text(family = "Righteous", colour = "#6675bd"))
-
-p <- ggpairs(data = penguins, columns = c(3, 4, 5, 9),
-        aes(colour = species)) +
-   +
-
-
-ggsave("2022/plots/work_in_progress/15_multivariate.png", p, dpi = 320, width = 12, height = 6)
-
-d1 <- nuclear_weapons %>% 
-  filter(!Entity %in% c("United States", "Russia", "United Kingdom", "France"))
-
-d2 <- nuclear_weapons %>% 
-  mutate(bin = cut_interval(nuclear_weapons_stockpile, length = 10000))
-
-ggplot(data = d2,
-       aes(x = Year, y = Entity, fill = bin)) +
-  geom_tile()
-
-boxplot(nuclear_weapons$Year ~ nuclear_weapons$Code)
-
-head(nuclear_weapons)
-
-ggplot(data = nuclear_weapons,
-       mapping = aes(x = Year, y = nuclear_weapons_stockpile,
-                     colour = Entity)) +
-  geom_line()
-
-# Data wrangling ----
-
-d1 <- tibble(
-  quartet_nb = rep(1:4, each = 11),
-  x = c(10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5,
-        8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8),
-  y = c(8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68,
-        9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74,
-        7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73,
-        6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89))
-
-# Create plot ----
-
-p1 <- ggplot(data = d1 %>% filter(quartet_nb == 1)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#ffa500") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-  
-p2 <- ggplot(data = d1 %>% filter(quartet_nb == 2)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#0099ff") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p3 <- ggplot(data = d1 %>% filter(quartet_nb == 3)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#009e73") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p4 <- ggplot(data = d1 %>% filter(quartet_nb == 4)) +
-  scale_x_continuous(limits = c(4, 19), breaks = seq(4, 18, 2)) +
-  scale_y_continuous(limits = c(4, 14), breaks = seq(4, 12, 2)) +
-  geom_segment(x = 0, xend = 20, y = 3, yend = 13,
-               colour = "#000000", size = 0.05) +
-  geom_point(aes(x = x, y = y),
-             size = 2, colour = "#b32db5") +
-  theme_minimal() +
-  theme(plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-        axis.line = element_line(colour = "black"),
-        panel.grid = element_blank(),
-        axis.title = element_text(family = "Tangerine", size = 50),
-        axis.text = element_text(family = "Tangerine", size = 40))
-
-p <- p1 + p2 + p3 + p4 +
-  plot_annotation(
-    title = "Anscombe's quartet",
-    subtitle = "Four datasets with nearly identical descriptive statistics but very different distributions",
-    caption = "Visualisation: Jonathan Kitt | Data source: www.cyclinglocations.com | #30DayChartChallenge 2022 | Day 3: historical",
-    theme = theme(plot.title = element_text(family = "Tangerine", colour = "black", size = 120, hjust = 0.5,
-                                            margin = margin(t = 20, b = 10)),
-                  plot.subtitle = element_text(family = "Tangerine", colour = "black", size = 75, hjust = 0.5,
-                                               margin = margin(b = 20)),
-                  plot.background = element_rect(fill = "#f3ddc2", colour = "#f3ddc2"),
-                  plot.caption = element_text(colour = "black", hjust = 0.5, size = 25)))
-
-ggsave("2022/plots/03_historical.png", p, dpi = 320, width = 12, height = 6)
