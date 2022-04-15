669eab3751c2706b59914016e07628fc6578313d
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-04-15 15:03:04 +0200

    day 17 plot ok

--- 2022/R/17_connections.R
+++ 2022/R/17_connections.R
@@ -7,55 +7,135 @@
 # Load packages ----
 
 library(osmdata)
+library(patchwork)
 library(showtext)
 library(tidyverse)
 
-# London subway ----
+# Load fonts ----
 
-subway <- getbb("London") %>% 
+font_add_google("Turret Road", "Turret Road")
+showtext_auto()
+
+# Data wrangling ----
+
+lille <- getbb("Lille") %>% 
   opq() %>% 
   add_osm_feature(key = "route",
                   value = "subway") %>% 
   osmdata_sf()
 
-ggplot() +
-  geom_sf(data = subway$osm_lines,
-          inherit.aes = FALSE,
-          size = 0.2,
-          colour = "white") 
-
-
-# Load fonts ----
-
-font_add_google("Eagle Lake", "Eagle Lake")
-showtext_auto()
-
-# Data wrangling ----
-
-edin_buses <- getbb("Edinburgh") %>% 
+lyon <- getbb("Lyon") %>% 
   opq() %>% 
   add_osm_feature(key = "route",
-                  value = "bus") %>% 
+                  value = "subway") %>% 
+  osmdata_sf()
+
+marseille <- getbb("Marseille") %>% 
+  opq() %>% 
+  add_osm_feature(key = "route",
+                  value = "subway") %>% 
+  osmdata_sf()
+
+paris <- getbb("Paris") %>% 
+  opq() %>% 
+  add_osm_feature(key = "route",
+                  value = "subway") %>% 
+  osmdata_sf()
+
+rennes <- getbb("Rennes") %>% 
+  opq() %>% 
+  add_osm_feature(key = "route",
+                  value = "subway") %>% 
+  osmdata_sf()
+
+toulouse <- getbb("Toulouse") %>% 
+  opq() %>% 
+  add_osm_feature(key = "route",
+                  value = "subway") %>% 
   osmdata_sf()
 
 # Create plot ----
 
-p <- ggplot() +
-  geom_sf(data = edin_buses$osm_lines,
+p1 <- ggplot() +
+  geom_sf(data = lille$osm_lines,
           inherit.aes = FALSE,
-          size = 0.2,
+          size = 0.25,
           colour = "white") +
-  xlim(c(3.32, 3.1)) +
-  ylim(c(55.895, 55.99)) +
-  ggtitle("EDINBURGH BUSES") +
-  labs(caption = "Visualisation: Jonathan Kitt | Data source: OpenStreetMap | #30DayChartChallenge 2022 | Day 17: connections") +
+  ggtitle(label = "Lille") +
   theme_void() +
   theme(panel.background = element_rect(fill = "#284c5d", colour = "#284c5d"),
         plot.background = element_rect(fill = "#284c5d", colou = "#284c5d"),
-        plot.title = element_text(family = "Eagle Lake", hjust = 0.5,
-                                  colour = "white", size = 75, margin = margin(t = 20)),
-        plot.caption = element_text(colour = "white", hjust = 0.5,
-                                    margin = margin(b = 15, t = 20), size = 25))
+        plot.title = element_text(family = "Turret Road", colour = "white", size = 35, hjust = 0),
+        plot.margin = margin(t = 10, b = 10, l = 10, r = 10))
+
+p2 <- ggplot() +
+  geom_sf(data = lyon$osm_lines,
+          inherit.aes = FALSE,
+          size = 0.25,
+          colour = "white") +
+  ggtitle(label = "Lyon") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#284c5d", colour = "#284c5d"),
+        plot.background = element_rect(fill = "#284c5d", colou = "#284c5d"),
+        plot.title = element_text(family = "Turret Road", colour = "white", size = 35, hjust = 0),
+        plot.margin = margin(t = 10, b = 10, l = 10, r = 10))
+
+p3 <- ggplot() +
+  geom_sf(data = marseille$osm_lines,
+          inherit.aes = FALSE,
+          size = 0.25,
+          colour = "white") +
+  ggtitle(label = "Marseille") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#284c5d", colour = "#284c5d"),
+        plot.background = element_rect(fill = "#284c5d", colou = "#284c5d"),
+        plot.title = element_text(family = "Turret Road", colour = "white", size = 35, hjust = 0),
+        plot.margin = margin(t = 10, b = 10, l = 10, r = 10))
+
+p4 <- ggplot() +
+  geom_sf(data = paris$osm_lines,
+          inherit.aes = FALSE,
+          size = 0.25,
+          colour = "white") +
+  ggtitle(label = "Paris") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#284c5d", colour = "#284c5d"),
+        plot.background = element_rect(fill = "#284c5d", colou = "#284c5d"),
+        plot.title = element_text(family = "Turret Road", colour = "white", size = 35, hjust = 0),
+        plot.margin = margin(t = 10, b = 10, l = 10, r = 10))
+
+p5 <- ggplot() +
+  geom_sf(data = rennes$osm_lines,
+          inherit.aes = FALSE,
+          size = 0.25,
+          colour = "white") +
+  ggtitle(label = "Rennes") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#284c5d", colour = "#284c5d"),
+        plot.background = element_rect(fill = "#284c5d", colou = "#284c5d"),
+        plot.title = element_text(family = "Turret Road", colour = "white", size = 35, hjust = 0),
+        plot.margin = margin(t = 10, b = 10, l = 10, r = 10))
+
+p6 <- ggplot() +
+  geom_sf(data = toulouse$osm_lines,
+          inherit.aes = FALSE,
+          size = 0.25,
+          colour = "white") + 
+  ggtitle(label = "Toulouse") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#284c5d", colour = "#284c5d"),
+        plot.background = element_rect(fill = "#284c5d", colou = "#284c5d"),
+        plot.title = element_text(family = "Turret Road", colour = "white", size = 35, hjust = 0),
+        plot.margin = margin(t = 10, b = 10, l = 10, r = 10))
+
+p <- p1 + p2 + p3 + p4 + p5 + p6 +
+  plot_annotation(
+    title = "Subways in french cities",
+    caption = "Visualisation: Jonathan Kitt | Data source: OpenStreetMap | #30DayChartChallenge 2022 | Day 17: connections",
+    theme = theme(plot.title = element_text(family = "Turret Road", colour = "white", size = 75, hjust = 0.5,
+                                            margin = margin(t = 10, b = 40)),
+                  plot.background = element_rect(fill = "#284c5d", colour = "#284c5d"),
+                  plot.caption = element_text(colour = "white", hjust = 0.5, size = 25)))
 
 # Save plot ----
 
