ee3fd4390abdf98a3131652a9cf008ff4efc386e
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-04-15 14:04:00 +0200

    day 16 plot ok

--- 2022/R/16_environment.R
+++ 2022/R/16_environment.R
@@ -12,14 +12,12 @@
 library(countrycode)
 library(tidyverse)
 library(showtext)
-# library(ggwaffle)
-# library(emojifont)
 library(patchwork)
 
 # Load fonts ----
 
-# font_add_google("Tangerine", "Tangerine")
-# showtext_auto()
+font_add_google("Gruppo", "Gruppo")
+showtext_auto()
 
 # Import data ----
 
@@ -33,33 +31,7 @@ country_list <- country_list %>%
 
 d1 <- co2 %>% 
   filter(Code %in% country_list$iso3c,
-         Year %in% c(1990, 2018))
-
-electricity <- d1 %>% 
-  select(code = Code, country = Entity, year = Year,
-         tons_person = `Electricity and heat (per capita)`) %>% 
-  pivot_wider(names_from = year,
-              values_from = tons_person) %>% 
-  rename(tons_person_1990 = `1990`,
-         tons_person_2018 = `2018`)
-
-buildings <- d1 %>% 
-  select(code = Code, country = Entity, year = Year,
-         tons_person = `Buildings (per capita)`) %>% 
-  pivot_wider(names_from = year,
-              values_from = tons_person) %>% 
-  rename(tons_person_1990 = `1990`,
-         tons_person_2018 = `2018`)
-
-transport <- d1 %>% 
-  select(code = Code, country = Entity, year = Year,
-         tons_person = `Transport (per capita)`) %>% 
-  pivot_wider(names_from = year,
-              values_from = tons_person) %>% 
-  rename(tons_person_1990 = `1990`,
-         tons_person_2018 = `2018`)
-
-aviation <- d1 %>% 
+         Year %in% c(1990, 2018)) %>% 
   select(code = Code, country = Entity, year = Year,
          tons_person = `Aviation and shipping (per capita)`) %>% 
   pivot_wider(names_from = year,
@@ -69,132 +41,35 @@ aviation <- d1 %>%
 
 # Create plot ----
 
-p1 <- ggplot(electricity, aes(x = tons_person_1990, y = tons_person_2018)) +
-  geom_segment(aes(x = 0, xend = 20, y = 0, yend = 20),
-               colour = "#1f9ce4") +
+p <- ggplot(d1, aes(x = tons_person_1990, y = tons_person_2018)) +
+  geom_segment(aes(x = 0, xend = 30, y = 0, yend = 30),
+               colour = "#1f9ce4", size = 0.25) +
   geom_point(shape = 16, colour = "#88f4ff", size = 2.5, alpha = 0.5) +
   labs(x = "1990", y = "2018",
-       title = "Electricity & heat",
-       subtitle = "Carbon dioxyde emissions in tons per person") +
+       title = "Carbon dioxyde emissions",
+       subtitle = "evolution of aviation- and shipping-related emissions between 1990 and 2018") +
+  annotate("text", x = 13.5, y = 31.5, family = "Gruppo", size = 15, colour = "#1f9ce4", hjust = 0,
+           label = "Singapore") +
+  annotate("text", x = 16.2, y = 7.77, family = "Gruppo", size = 15, colour = "#1f9ce4", hjust = 0,
+           label = "United Arab Emirates") +
+  annotate("text", x = 1.156, y = 17.2, family = "Gruppo", size = 15, colour = "#1f9ce4", hjust = 0,
+           label = "Malta") +
+  annotate("text", x = 4.82, y = 8.42, family = "Gruppo", size = 15, colour = "#1f9ce4", hjust = 0,
+           label = "Seychelles") +
+  annotate("text", x = 5, y = 25, family = "Gruppo", size = 20, colour = "#88f4ff", hjust = 0,
+           label = "countries above the line have increased their emissions") +
+  annotate("text", x = 10, y = 2, family = "Gruppo", size = 20, colour = "#88f4ff", hjust = 0,
+           label = "countries below the line have decreased their emissions") +
   theme(panel.background = element_rect(fill = "#242632", colour = "#242632"),
         plot.background = element_rect(fill = "#242632", colour = "#242632"),
         panel.grid = element_blank(),
-        axis.title = element_text(colour = "white"),
-        axis.text = element_text(colour = "white"),
-        plot.title = element_text(colour = "#1f9ce4"),
-        plot.subtitle = element_text(colour = "#1f9ce4"))
+        axis.title = element_text(family = "Gruppo", colour = "white", size = 40),
+        axis.text = element_text(family = "Gruppo", colour = "white", size = 30),
+        plot.title = element_text(family = "Gruppo", colour = "#1f9ce4", size = 100,
+                                  margin = margin(t = 10, b = 5)),
+        plot.subtitle = element_text(family = "Gruppo", colour = "#1f9ce4", size = 50,
+                                     margin = margin(b = 10)))
 
-p2 <- ggplot(buildings, aes(x = tons_person_1990, y = tons_person_2018)) +
-  geom_segment(aes(x = 0, xend = 4, y = 0, yend = 4),
-               colour = "#1f9ce4") +
-  geom_point(shape = 16, colour = "#88f4ff", size = 2.5, alpha = 0.5) +
-  labs(x = "1990", y = "2018",
-       title = "Buildings",
-       subtitle = "Carbon dioxyde emissions in tons per person") +
-  theme(panel.background = element_rect(fill = "#242632", colour = "#242632"),
-        plot.background = element_rect(fill = "#242632", colour = "#242632"),
-        panel.grid = element_blank(),
-        axis.title = element_text(colour = "white"),
-        axis.text = element_text(colour = "white"),
-        plot.title = element_text(colour = "#1f9ce4"),
-        plot.subtitle = element_text(colour = "#1f9ce4"))
-
-p3 <- ggplot(transport, aes(x = tons_person_1990, y = tons_person_2018)) +
-  geom_segment(aes(x = 0, xend = 10.5, y = 0, yend = 10.5),
-               colour = "#1f9ce4") +
-  geom_point(shape = 16, colour = "#88f4ff", size = 2.5, alpha = 0.5) +
-  labs(x = "1990", y = "2018",
-       title = "Transport",
-       subtitle = "Carbon dioxyde emissions in tons per person") +
-  theme(panel.background = element_rect(fill = "#242632", colour = "#242632"),
-        plot.background = element_rect(fill = "#242632", colour = "#242632"),
-        panel.grid = element_blank(),
-        axis.title = element_text(colour = "white"),
-        axis.text = element_text(colour = "white"),
-        plot.title = element_text(colour = "#1f9ce4"),
-        plot.subtitle = element_text(colour = "#1f9ce4"))
-
-p4 <- ggplot(aviation, aes(x = tons_person_1990, y = tons_person_2018)) +
-  geom_segment(aes(x = 0, xend = 32, y = 0, yend = 32),
-               colour = "#1f9ce4") +
-  geom_point(shape = 16, colour = "#88f4ff", size = 2.5, alpha = 0.5) +
-  labs(x = "1990", y = "2018",
-       title = "Aviation & shipping",
-       subtitle = "Carbon dioxyde emissions in tons per person") +
-  theme(panel.background = element_rect(fill = "#242632", colour = "#242632"),
-        plot.background = element_rect(fill = "#242632", colour = "#242632"),
-        panel.grid = element_blank(),
-        axis.title = element_text(colour = "white"),
-        axis.text = element_text(colour = "white"),
-        plot.title = element_text(colour = "#1f9ce4"),
-        plot.subtitle = element_text(colour = "#1f9ce4"))
-
-p <- p1 + p2 + p3
-
-ggsave("2022/plots/work_in_progress/16_environment.png", p, dpi = 320, width = 12, height = 6)
-
-
-ggplot(data = d1,
-       aes(x = category, y = percent, fill = fct_rev(location))) +
-  geom_bar(position = "stack", stat = "identity", show.legend = FALSE,
-           width = 0.25) +
-  coord_flip() +
-  scale_fill_manual(values = c("#1f9ce4", "#88f4ff", "#7339ab")) +
-  theme(panel.background = element_rect(fill = "#242632", colour = "#242632"),
-        plot.background = element_rect(fill = "#242632", colour = "#242632"))
-
-archaea <- biomass %>% 
-  filter(category == "Archaea")
-
-
-archaea.p1 <- ggplot(data = filter(archaea, location == "Terrestrial"),
-       aes(x = 6, y = percent, fill = location)) +
-  geom_bar(stat = "identity",
-           show.legend = FALSE) +
-  ylim(0, 100) +
-  scale_fill_manual(values = c("#7339ab", "#393c4b")) +
-  coord_polar(theta = "y", start = 0) +
-  xlim(c(0.025, 8)) +
-  annotate("text", x = 0.025, y = 0.5, label = "6 %", colour = "#7339ab", size = 60) +
-  theme_void() +
-  theme(panel.background = element_rect(fill = "#242632", colour = "#242632"),
-        plot.background = element_rect(fill = "#242632", colour = "#242632"))
-
-archaea.p2 <-
-  ggplot(data = filter(archaea, location == "Marine"),
-                     aes(x = 6, y = percent, fill = location)) +
-  geom_bar(stat = "identity",
-           show.legend = FALSE) +
-  ylim(0, 100) +
-  scale_fill_manual(values = c("#88f4ff", "#393c4b")) +
-  coord_polar(theta = "y", start = 0) +
-  xlim(c(0.025, 8)) +
-  annotate("text", x = 0.025, y = 0.5, label = "4 %", colour = "#88f4ff", size = 60) +
-  theme_void() +
-  theme(panel.background = element_rect(fill = "#242632", colour = "#242632"),
-        plot.background = element_rect(fill = "#242632", colour = "#242632"))
-
-archaea.p3 <-
-  ggplot(data = filter(archaea, location == "Deep subsurface"),
-         aes(x = 6, y = percent, fill = location)) +
-  geom_bar(stat = "identity",
-           show.legend = FALSE) +
-  ylim(0, 100) +
-  scale_fill_manual(values = c("#1f9ce4", "#393c4b")) +
-  coord_polar(theta = "y", start = 0) +
-  xlim(c(0.025, 8)) +
-  annotate("text", x = 0.025, y = 0.5, label = "90 %", colour = "#1f9ce4", size = 60) +
-  theme_void() +
-  theme(panel.background = element_rect(fill = "#242632", colour = "#242632"),
-        plot.background = element_rect(fill = "#242632", colour = "#242632"))
-
-p <- archaea.p1 + archaea.p2 + archaea.p3 +
-  archaea.p1 + archaea.p2 + archaea.p3 +
-  archaea.p1 + archaea.p2 + archaea.p3 +
-  archaea.p1 + archaea.p2 + archaea.p3 +
-  archaea.p1 + archaea.p2 + archaea.p3 +
-  archaea.p1 + archaea.p2 + archaea.p3 +
-  plot_layout(ncol = 3)
-p
+# Save plot ----
 
 ggsave("2022/plots/work_in_progress/16_environment.png", p, dpi = 320, width = 12, height = 6)
